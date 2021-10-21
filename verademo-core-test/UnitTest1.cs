14095b6544f24921bacdf4faa05b0b23c98b1e99
Dennis Medeiros <15147612+dennismedeiros@users.noreply.github.com>
2021-10-21 12:10:57 -0400

    Initial Commit of application and unit testing

---
+++ verademo-core-test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace verademo_core_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
